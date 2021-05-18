f967cc765f63c8376b77d1c16677957a70809cd3
Abikayusri <abikayusri96@gmail.com>
2021-05-18 23:59:48 +0700

    Configuration Midtrans transaction and create routes for Midtrans

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +23,8 @@ Route::middleware('auth:sanctum')->group(function(){
     
     Route::get('transaction', [TransactionController::class, 'all']);
     Route::post('transaction/{id}', [TransactionController::class, 'update']);
+
+    Route::post('checkout', [TransactionController::class, 'checkout']);
 });
 
 Route::post('login', [UserController::class, 'login']);
